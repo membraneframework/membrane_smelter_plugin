485e305828c3f7d13149fe29cb9964b5b0052858
Wojciech Kozyra <wkozyra95@gmail.com>
2025-11-28 18:25:07 +0100

    Add nix config

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Dev shell for plugin development";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, lib, ... }:
+        {
+          devShells = {
+            default = pkgs.mkShell {
+              env.LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
+                xorg.libX11
+                xorg.libXext
+                xorg.libXrandr
+                xorg.libXfixes
+                xorg.libXi
+                xorg.libXcursor
+                xorg.libXScrnSaver
+                alsa-lib
+                libopus
+                vulkan-loader
+              ]);
+
+              packages = with pkgs; [
+                libGL
+                ffmpeg
+                elixir
+              ];
+            };
+          };
+        };
+    };
+}
