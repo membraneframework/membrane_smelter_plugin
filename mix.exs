62c8e940b520d35c591fcf6716ae3dfb726ebd17
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2025-12-02 13:59:25 +0100

    Release `0.12.0`

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Smelter.Mixfile do
   use Mix.Project
 
-  @version "0.11.1"
+  @version "0.12.0"
   @github_url "https://github.com/membraneframework/membrane_smelter_plugin"
 
   def project do
