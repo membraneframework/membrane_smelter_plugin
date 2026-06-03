925f506a728074e2ac42ba05cc0b10d7bca360f3
Łukasz Kita <lukasz.kita0@gmail.com>
2026-06-03 10:53:41 +0200

    Bump membrane_rtp_plugin

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule Membrane.Smelter.Mixfile do
       {:membrane_raw_video_format, "~> 0.3.0 or ~> 0.4.0"},
       {:membrane_opus_plugin, "~> 0.20.4"},
       ## RTP
-      {:membrane_rtp_plugin, "0.31.3"},
+      {:membrane_rtp_plugin, "~> 0.31.4"},
       {:membrane_rtp_h264_plugin, "~> 0.20.0"},
       {:membrane_tcp_plugin, "~> 0.6.0"},
       {:membrane_rtp_opus_plugin, "~> 0.10.0"},
