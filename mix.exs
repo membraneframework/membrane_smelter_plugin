d6a2c4efbacb54db92e81d9514ab169845cd8ff5
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 12:13:48 +0200

    Rename prepend_llms_links to append_llms_links

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Membrane.Smelter.Mixfile do
       name: "Membrane Smelter Plugin",
       source_url: @github_url,
       docs: docs(),
-      aliases: [docs: ["docs", &prepend_llms_links/1]]
+      aliases: [docs: ["docs", &append_llms_links/1]]
     ]
   end
 
@@ -108,7 +108,7 @@ defmodule Membrane.Smelter.Mixfile do
     ]
   end
 
-  defp prepend_llms_links(_) do
+  defp append_llms_links(_args) do
     output_dir = docs()[:output] || "doc"
     path = Path.join(output_dir, "llms.txt")
 
